f6558d72df846989a7ee552a61b3b21aa967fe8a
Emanuela <83732293+desmavi@users.noreply.github.com>
2022-04-01 21:19:26 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import PasswordFields from './components/PasswordFields'
+import useGeneratePasswords from "./hooks/useGeneratePasswords"
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+    const { passwords, setPasswordArray, passwordPlaceholders} = useGeneratePasswords()
+    
+    return (
+        <main>
+            <div id="container">
+                <div id="header">
+                    <h1>Generate a <span className="green">random password</span></h1>
+                    <p>Never use an insecure password again.</p>
+                    <button className="generate-btn" onClick={setPasswordArray}>
+                        <i className="bi bi-lightning-charge margin"></i>
+                        Generate passwords
+                    </button>
+                </div>
+                <hr/>
+                <div id="passwords-container">
+                    {passwords.length > 0 ? 
+                    <PasswordFields passwords={passwords}/> : passwordPlaceholders}
+                </div>
+            </div>
+        </main>
+    )
 }
-
-export default App;
